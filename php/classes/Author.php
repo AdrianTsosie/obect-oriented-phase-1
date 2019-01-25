e0bc6c291c529fb43fbabdc6791d8fde68fcc44f
Adian Tsosie <addve12@yahoo.com>
2019-01-25 11:55:29 -0700

    Worked on Author.php

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -2,44 +2,46 @@
 namespace Atsosie11/Author;
 
 require_once ("autoload.php");
-require_one (
+require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
 
-
-)
-
+use Ramsey\Uuid\Uuid;
 /**
- * Typical profile for website
- *
- *This profile is an abbreviated sample
+ * Typical profile for author profile.
  *
  * @Author Adrian Tsosie <atsosie11@cnm.edu>
+ * @version 3.0.0
  */
-elass Author {
+class Author {
 	/**
 	 * id for the Author name.
+	 * @var Uuid authorId
 	 */
-	private @authorId;
+	private $authorId;
 	/**
- 	* url target for account login
+	 * url target for account login
+	 * @var string authorAvatorUrl
 	 */
-	private @authorAvatarUrl;
+	private $authorAvatarUrl;
 	/**
 	 * login for authors account
+	 * @var string authorActivationToken
 	 */
-	private @authorActivationToken;
+	private $authorActivationToken;
 	/**
 	 * email address for author
+	 * @var string authorEmail
 	 */
-	private @authorEmail;
-/**
- * has tag for the user
- */
-	private @authorHash;
-/**
- * the user name for the account
- */
-	private @authorUsername;
-
+	private $authorEmail;
+	/**
+	 * has tag for the user
+	 * @var string authorHas
+ 	**/
+	private $authorHash;
+	/**
+	 * the user name for the account
+	 * @var string authorUsername
+ 	*/
+	private $authorUsername;
 	/**
 	 * accessor method for author id
 	 *
@@ -54,24 +56,57 @@ elass Author {
 	 *
 	 * @param  Uuid| string $newAuthorId value of new author id
 	 * @throws \RangeException if $newAuthorId is not positive
-	 */
+	 **/
 
 	/**
-	 * accessor method for account activation token
+	 * accessor method for author avatar url
 	 *
-	 * @return string value of the activation token
+	 * @return string value of the author avatar url
 	 */
-	public function getProfileActivationToken() : ?string {
-		return ($this->profileActivationToken);
+	public function getAuthorAvatarUrl() : ?string {
+		return ($this->authorAvatarUrl);
 	}
 	/**
-	 * mutator method for account activation token
+	 * mutator method for author avatar url
 	 *
-	 * @param string $newProfileActivationToken
-	 * @throws \InvalidArgumentException  if the token is not a string or insecure
+	 * @param string $newAuthorAvatarUrl
 	 * @throws \RangeException if the token is not exactly 32 characters
 	 * @throws \TypeError if the activation token is not a string
 	 */
+
+	/**
+	 * accessor method for author activation token
+	 *
+	 * @return string value of the author activation token
+	 */
+	public function getAuthorActivationToken() : ?string {
+		return ($this->authorActivationToken);
+	}
+	/**
+	 * mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken
+	 * @throws \RangeException if the token is not exactly 32 characters
+	 * @throws \TypeError if the activation token is not a string
+	 */
+
+	/**
+	 * accessor method for author email
+	 *
+	 * @return string value of author email
+	 **/
+	public function getAuthorEmail(): string {
+		return ($this->authorEmail);
+	}
+	/**
+	 * mutator method for at handle
+	 *
+	 * @param string $newProfileAtHandle new value of at handle
+	 * @throws \InvalidArgumentException if $newAtHandle is not a string or insecure
+	 * @throws \RangeException if $newAtHandle is > 32 characters
+	 * @throws \TypeError if $newAtHandle is not a string
+	 **/
+
 	public function setProfileActivationToken(?string $newProfileActivationToken): void {
 		if($newProfileActivationToken === null) {
 			$this->profileActivationToken = null;
@@ -87,21 +122,6 @@ elass Author {
 		}
 		$this->profileActivationToken = $newProfileActivationToken;
 	}
-	/**
-	 * accessor method for at handle
-	 *
-	 * @return string value of at handle
-	 **/
-	public function getProfileAtHandle(): string {
-		return ($this->profileAtHandle);
-	}
-	/**
-	 * mutator method for at handle
-	 *
-	 * @param string $newProfileAtHandle new value of at handle
-	 * @throws \InvalidArgumentException if $newAtHandle is not a string or insecure
-	 * @throws \RangeException if $newAtHandle is > 32 characters
-	 * @throws \TypeError if $newAtHandle is not a string
-	 **/
+
 }
 
